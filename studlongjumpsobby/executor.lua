568d562705bdb8c42109beab5ef530c30134b97e
eyesing <77251330+mlnitoon2@users.noreply.github.com>
2025-02-04 18:08:28 -0600

    Update executor.lua

--- studlongjumpsobby/executor.lua
+++ studlongjumpsobby/executor.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/mlnitoon2/ROK/refs/heads/main/studlongjumpsobby/main.lua"))
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mlnitoon2/ROK/refs/heads/main/studlongjumpsobby/main.lua"))()
