8c8d6aa8b2f45d5adf92d8b5f6a7c2808df13c2f
eyesing <77251330+mlnitoon2@users.noreply.github.com>
2025-02-08 00:50:48 -0600

    Update main.lua

--- rokfe/main.lua
+++ rokfe/main.lua
@@ -1,3 +1,19 @@
+--[[
+	Free Version:
+	https://www.roblox.com/catalog/4645404679/International-Fedora-Thailand
+	https://www.roblox.com/catalog/3662265036/International-Fedora-Indonesia
+	https://www.roblox.com/catalog/4622081834/International-Fedora-China
+	https://www.roblox.com/catalog/3992084515/International-Fedora-Vietnam
+	https://www.roblox.com/catalog/4819740796/Robox
+
+	Accurate Version:
+	https://www.roblox.com/catalog/14255560646/Extra-Left-Tan-Arm
+	https://www.roblox.com/catalog/14255562939/Extra-Right-Tan-Arm
+	https://www.roblox.com/catalog/17374846953/Extra-Right-Black-Arm
+	https://www.roblox.com/catalog/17374851733/Extra-Left-Black-Arm
+	https://www.roblox.com/catalog/13421786478/Extra-Torso-Blocky
+]]
+
 Configuration = {
 	ReturnOnDeath = true,
 	Flinging = true,
@@ -9,35 +25,11 @@ Configuration = {
 	AntiVoiding = true,
 	SetSimulationRadius = true,
 	DisableCharacterScripts = true,
+	RigName = "ROKFE",
 	AccessoryFallbackDefaults = true,
 	OverlayFakeCharacter = false,
 	
-	Hats = {
-	["Right Arm"] = {
-		{Texture = "14255544465", Mesh = "14255522247", Name = "RARM", Offset = CFrame.Angles(0, 0, math.rad(90))},
-		{Texture = "4645402630", Mesh = "3030546036", Name = "International Fedora", Offset = CFrame.Angles(math.rad(-90), 0, math.rad(90))}
-	},
-
-	["Left Arm"] = {
-		{Texture = "14255544465", Mesh = "14255522247", Name = "LARM", Offset = CFrame.Angles(0, 0, math.rad(90))},
-		{Texture = "3650139425", Mesh = "3030546036", Name = "International Fedora", Offset = CFrame.Angles(math.rad(-90), 0, math.rad(-90))}
-	},
-
-	["Right Leg"] = {
-		{Texture = "17374768001", Mesh = "17374767929", Name = "Accessory (RARM)", Offset = CFrame.Angles(0, 0, math.rad(90))},
-		{Texture = "4622077774", Mesh = "3030546036", Name = "International Fedora", Offset = CFrame.Angles(math.rad(-90), 0, math.rad(90))}
-	},
-
-	["Left Leg"] = {
-		{Texture = "17374768001", Mesh = "17374767929", Name = "Accessory (LARM)", Offset = CFrame.Angles(0, 0, math.rad(90))},
-		{Texture = "3860099469", Mesh = "3030546036", Name = "InternationalFedora", Offset = CFrame.Angles(math.rad(-90), 0, math.rad(-90))}
-	},	
-
-	["Torso"] = {
-		{Texture = "131014325980101", Mesh = "127552124837034", Name = "Accessory (Toorso)", Offset = CFrame.new(0,0.133,0) * CFrame.fromOrientation(math.rad(0), math.rad(-180), math.rad(0))},
-		{Texture = "4819722776", Mesh = "4819720316", Name = "MeshPartAccessory", Offset = CFrame.Angles(0, 0, math.rad(-15))}
-	},
-}, -- Set to nil if you want to use defaults.
+	Hats = nil, -- Set to nil if you want to use defaults.
 }
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/KadeTheExploiter/Krypton/main/Module.luau"))()
